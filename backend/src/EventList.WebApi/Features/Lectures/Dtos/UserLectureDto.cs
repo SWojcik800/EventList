cc1390bf186569cf70729c8ce85dfa8de1da034d
Szymon <szymonwojcik800@gmail.com>
2023-04-23 17:24:45 +0200

    Add retrieving lectures list

---
+++ backend/src/EventList.WebApi/Features/Lectures/Dtos/UserLectureDto.cs
@@ -0,0 +1,26 @@
+﻿using EventList.WebApi.Common.Mappings;
+using EventList.WebApi.Entities;
+using EventList.WebApi.ValueObjects;
+
+namespace EventList.WebApi.Features.Lectures.Dtos
+{
+    public sealed class UserLectureDto
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+
+        public IList<string?> LecturerNames { get; set; }
+
+        public Location Location { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration => EndTime - StartTime;
+
+        public string? Name { get; set; }
+
+        public string? Topic { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ backend/src/EventList.WebApi/Features/Lectures/Mapping/LectureMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EventList.WebApi.Entities;
+using EventList.WebApi.Features.Lectures.Dtos;
+
+namespace EventList.WebApi.Features.Lectures.Mapping
+{
+    public sealed class LectureMapping : Profile
+    {
+
+        public LectureMapping()
+        {
+            CreateMap<Lecture, UserLectureDto>()
+                .ForMember(dest => dest.LecturerNames, opt => opt.MapFrom(x => x.Lecturers.Select(x => x.Name).ToList()))
+                .ForMember(dest => dest.Duration, opt => opt.Ignore());
+        }
+    }
+}
