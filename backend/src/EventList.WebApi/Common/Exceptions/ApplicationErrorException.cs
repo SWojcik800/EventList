ce88a6cf1bae4432c32e874e92f62ccd3f33396c
Szymon <szymonwojcik800@gmail.com>
2023-04-23 16:03:06 +0200

    Add normalization of returning error response

---
+++ backend/src/EventList.WebApi/Common/Exceptions/ApplicationErrorException.cs
@@ -0,0 +1,12 @@
+﻿namespace EventList.WebApi.Common.Exceptions
+{
+    /// <summary>
+    /// Represents application error
+    /// </summary>
+    public class ApplicationErrorException : Exception
+    {
+        public ApplicationErrorException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ backend/src/EventList.WebApi/Web/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using EventList.WebApi.Common.Exceptions;
+using Newtonsoft.Json;
+
+namespace EventList.WebApi.Web.Middleware
+{
+    /// <summary>
+    /// Error handling middleware for web api
+    /// </summary>
+    public sealed class ErrorHandlingMiddleware : IMiddleware
+    {
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException e)
+            {
+                await WriteErrorToBody(context, e.Message, 404);
+            }
+            catch (ForbiddenAccessException e)
+            {
+                await WriteErrorToBody(context, e.Message, 403);
+            }
+            catch (ApplicationErrorException e)
+            {
+                await WriteErrorToBody(context, e.Message, 500);
+            }
+
+        }
+
+        private async Task WriteErrorToBody(HttpContext context, string errorMessage, int statusCode)
+        {
+            context.Response.StatusCode = statusCode;
+            var apiError = new ErrorApiResponse(errorMessage);
+
+            await context.Response.WriteAsJsonAsync(apiError);
+        }
+    }
+}
