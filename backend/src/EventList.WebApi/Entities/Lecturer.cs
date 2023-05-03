d392449a050dc6536981c0382925d5b0b956a999
JakubPloch <kubaploch34@gmail.com>
2023-05-03 12:44:10 +0200

    Add lecturer creation

--- backend/src/EventList.WebApi/Entities/Lecturer.cs
+++ backend/src/EventList.WebApi/Entities/Lecturer.cs
@@ -2,16 +2,25 @@
 
 namespace EventList.WebApi.Entities
 {
-    public class Lecturer : AuditableEntity
+    public sealed class Lecturer : AuditableEntity
     {
+        private Lecturer()
+        {
+            //For EF Core 
+        }
+
+        public Lecturer(string? name, string? description)
+        {
+            Name = name;
+            Description = description;
+        }
+
         public int Id { get; set; }
 
-        public IList<Lecture> Lectures { get; set; }
+        public IList<Lecture> Lectures { get; private set; }
 
         public string? Name { get; set; }
 
         public string? Description { get; set; }
-        
-
     }
 }
