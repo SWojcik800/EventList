46c6ee175031ffb73ce95730518b520024225907
SWojcik800 <43492034+SWojcik800@users.noreply.github.com>
2023-05-20 20:17:41 +0200

    Fix refreshing after returning to view (#21)

--- frontend/EventListMobileApp/app/src/main/java/com/example/eventlistmobileapp/SecondFragment.kt
+++ frontend/EventListMobileApp/app/src/main/java/com/example/eventlistmobileapp/SecondFragment.kt
@@ -1,10 +1,13 @@
 package com.example.eventlistmobileapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager.OnActivityResultListener
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.eventlistmobileapp.databinding.FragmentSecondBinding
 
@@ -18,6 +21,7 @@ class SecondFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var _currentActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +29,7 @@ class SecondFragment : Fragment() {
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
         return binding.root
 
     }
@@ -32,10 +37,15 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        _currentActivity = requireActivity() as MainActivity
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
