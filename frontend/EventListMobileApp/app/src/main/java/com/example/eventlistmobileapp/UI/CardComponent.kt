75e1c1387b55253e3e92a2d8675600cb48a9700f
Szymon <szymonwojcik800@gmail.com>
2023-05-20 18:22:37 +0200

    Add generic card list

--- frontend/EventListMobileApp/app/src/main/java/com/example/eventlistmobileapp/UI/CardComponent.kt
+++ frontend/EventListMobileApp/app/src/main/java/com/example/eventlistmobileapp/UI/CardComponent.kt
@@ -3,7 +3,12 @@ package com.example.eventlistmobileapp.UI
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.ArrayAdapter
 import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.ListView
+import com.example.eventlistmobileapp.Lectures.Lecture
+import com.example.eventlistmobileapp.R
 import com.example.eventlistmobileapp.databinding.CardComponentBinding
 
 class CardComponent @JvmOverloads constructor(
@@ -18,15 +23,29 @@ class CardComponent @JvmOverloads constructor(
         binding = CardComponentBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
-    fun setTitle(title: String) {
-        binding.cardTitle.text = title
-    }
 
-    fun setDescription(description: String) {
-        binding.cardDescription.text = description
+    fun setPropertiesFrom(cardItems: List<CardComponentItem>): CardComponent
+    {
+        createList(cardItems)
+        return this
+    }
+    fun setTitle(title: String): CardComponent {
+        binding.cardTitle.text = title
+        return this
     }
 
     fun setCardOnClickListener(listener: OnClickListener) {
         binding.cardContainer.setOnClickListener(listener)
+    }
+
+    private fun createList(items: List<CardComponentItem>)
+    {
+        var listView = findViewById<ListView>(R.id.cardListItems);
+        val mappedItems = items.map { item -> "${item.key}: ${item.value}" }.toList()
+
+        val arrayAdapter = ArrayAdapter(this.context,
+            android.R.layout.simple_list_item_1, mappedItems)
+
+        listView.adapter = arrayAdapter
     }
 }
